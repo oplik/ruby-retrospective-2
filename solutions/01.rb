bd2cbffd7a5c2cde9bae2160c3ccb51e70463b99
oplik <mihail.jekov@gmail.com>
2011-11-14 14:42:46 +0200

    Update solutions/01.rb

---
+++ solutions/01.rb
@@ -0,0 +1,25 @@
+class Array
+  def to_hash
+		Hash[*self.flatten(1)]
+	end
+	def index_by
+		inject({}) do |result, current| 
+			result[yield(current)] = current
+			result
+		end
+	end
+	def subarray_count(substr)
+		#мисля, че е малко грозно, но на мен ми харесва, хаха
+		substr = substr.join if((string = self.join) and substr.is_a?(Array))
+		count = 0
+		count+=1 while(string.include?(substr) and string.sub!(substr, substr[1..-1]))
+		count
+	end
+	def occurences_count
+		h = Hash.new(0)
+		inject(h) do |result, current| 
+			result[current]+=1
+			result
+		end
+	end
+end
